178b1a65cb6d255a73adcac3312a89b025f2c04d
obi ugochukwu <obiugochukwu003@gmail.com>
2024-07-03 11:01:12 -0700

    update four

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next)=>{
     next();
 })
 app.use('/api/auth/', authRouter)
-app.use('https://api-i248.onrender.com/todo/', todoRouter)
+app.use('/api-i248.onrender.com/todo/', todoRouter)
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
